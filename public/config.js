428f0574f54d1825fe169acd4b85942753ae3339
John1414141 <j14324@mail.i-dork.com>
2024-01-22 08:15:13 -0500

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,9 +1,9 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "CHANGEME",
+  VITE_CORS_PROXY_URL: "1569678a3fd7f699bb647a6607de2e2a",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "CHANGEME",
+  VITE_TMDB_READ_API_KEY: "worker-shiny-king-e4a7.j14324.workers.dev",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
